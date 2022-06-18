397ba4c94a7640fd8305d8b2a0b626963eb87c79
robbie632 <rob.632@hotmail.co.uk>
2022-06-18 10:17:29 +0100

    first commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+# followed these instructions: https://zetcode.com/gui/qt5/introduction/
+
+# run belo wline on its own first, then add QT += widgets to bottom of generated .pro file 
+# then run last two lines from this file
+#/usr/lib/qt5/bin/qmake -project
+/usr/lib/qt5/bin/qmake 
+make
